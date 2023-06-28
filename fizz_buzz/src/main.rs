eec08a1a4d7a1ab1a594c168c8dc17b0b7a8197a
ladaoot <ladachumakova@yandex.ru>
2023-06-28 14:05:13 +0300

    homework 1 FizzBuzz

---
+++ fizz_buzz/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    for i in 1..=100{
+        match (i%3 == 0, i%5 ==0) {
+            (true, true) => println!("FizzBuzz"),
+            (true, false) => println!("Fizz"),
+            (false, true) => println!("Buzz"),
+            _ => println!("{i}")
+        };
+    };
+
+}
