defa0dc05097482b887ce14d82d2b4e627641cf2
ladaoot <ladachumakova@yandex.ru>
2023-07-09 15:00:30 +0300

    chapter 7

---
+++ chapter7/src/lib.rs
@@ -0,0 +1,11 @@
+// use std::fmt::Result;
+// // as создает псевдоним по которомы мы можем обращаться к библиотеке\структуре\функции\модулю
+// use std::io::Result as IoResult;
+//
+// fn function1() -> Result {
+//     // --snip--
+// }
+//
+// fn function2() -> IoResult<()> {
+//     // --snip--
+// }
