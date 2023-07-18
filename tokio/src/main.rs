0ed0218f580b92b19930afe24e848be48b189add
ladaoot <ladachumakova@yandex.ru>
2023-07-18 11:33:12 +0300

    tokio async in depth

--- tokio/src/main.rs
+++ tokio/src/main.rs
@@ -69,36 +69,196 @@
 //     io::copy(&mut reader, &mut file).await?;
 //     Ok(())
 // }
+//
+// use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
+// use tokio::net::TcpStream;
+//
+// #[tokio::main]
+// async fn main() -> io::Result<()> {
+//     let socket = TcpStream::connect("127.0.0.1:6142").await?;
+//     let (mut rd, mut wr) = io::split(socket);
+//
+//     // Write data in the background
+//     tokio::spawn(async move {
+//         wr.write_all(b"hello\r\n").await?;
+//         wr.write_all(b"world\r\n").await?;
+//
+//         // Sometimes, the rust type inferencer needs
+//         // a little help
+//         Ok::<_, io::Error>(())
+//     });
+//
+//     let mut buf = vec![0; 128];
+//
+//     loop {
+//         let n = rd.read(&mut buf).await?;
+//
+//         if n == 0 {
+//             break;
+//         }
+//
+//         println!("GOT {:?}", &buf[..n]);
+//     }
+//
+//     Ok(())
+// }
 
-use tokio::io::{self, AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpStream;
+// use tokio::net::TcpStream;
+//
+// async fn my_async_fn() {
+//     println!("hello from async");
+//     let _socket = TcpStream::connect("127.0.0.1:8888").await.unwrap();
+//     println!("async TCP operation complete");
+// }
+//
+// #[tokio::main]
+// async fn main() {
+//     let what_is_this = my_async_fn();
+//     // Nothing has been printed yet.
+//     what_is_this.await;
+//     // Text has been printed and socket has been
+//     // established and closed.
+// }
+
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::time::{Duration, Instant};
+use std::collections::VecDeque;
+use std::thread;
+use futures::task;
+
+
+struct Delay {
+    when: Instant,
+}
+
+impl Future for Delay {
+    type Output = &'static str;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>)
+        -> Poll<&'static str>
+    {
+        if Instant::now() >= self.when {
+            println!("Hello world");
+            Poll::Ready("done")
+        } else {
+            // Get a handle to the waker for the current task
+            let waker = cx.waker().clone();
+            let when = self.when;
+
+            // Spawn a timer thread.
+            thread::spawn(move || {
+                let now = Instant::now();
+
+                if now < when {
+                    thread::sleep(when - now);
+                }
+
+                waker.wake();
+            });
+
+            Poll::Pending
+        }
+    }
+}
+
+
+enum MainFuture {
+    // Initialized, never polled
+    State0,
+    // Waiting on `Delay`, i.e. the `future.await` line.
+    State1(Delay),
+    // The future has completed.
+    Terminated,
+}
+
+impl Future for MainFuture {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>)
+            -> Poll<()>
+    {
+        use MainFuture::*;
+
+        loop {
+            match *self {
+                State0 => {
+                    let when = Instant::now() +
+                        Duration::from_millis(10);
+                    let future = Delay { when };
+                    *self = State1(future);
+                    // println!("State0");
+                }
+                State1(ref mut my_future) => {
+                    match Pin::new(my_future).poll(cx) {
+                        Poll::Ready(out) => {
+                            // println!("State1");
+                            assert_eq!(out, "done");
+                            *self = Terminated;
+
+                            return Poll::Ready(());
+                        }
+                        Poll::Pending => {
+                            // println!("Pen");
+                            return Poll::Pending;
+                        }
+                    }
+                }
+                Terminated => {
+                    // println!("Terminated");
+                    panic!("future polled after completion")
+                }
+            }
+        }
+    }
+}
+
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
-    let socket = TcpStream::connect("127.0.0.1:6142").await?;
-    let (mut rd, mut wr) = io::split(socket);
+fn main() {
+    let mut mini_tokio = MiniTokio::new();
 
-    // Write data in the background
-    tokio::spawn(async move {
-        wr.write_all(b"hello\r\n").await?;
-        wr.write_all(b"world\r\n").await?;
+    mini_tokio.spawn(async {
+        let when = Instant::now() + Duration::from_millis(10);
+        let future = Delay { when };
 
-        // Sometimes, the rust type inferencer needs
-        // a little help
-        Ok::<_, io::Error>(())
+        let out = future.await;
+        assert_eq!(out, "done");
     });
 
-    let mut buf = vec![0; 128];
+    mini_tokio.run();
+}
 
-    loop {
-        let n = rd.read(&mut buf).await?;
+struct MiniTokio {
+    tasks: VecDeque<Task>,
+}
 
-        if n == 0 {
-            break;
+type Task = Pin<Box<dyn Future<Output=()> + Send>>;
+
+impl MiniTokio {
+    fn new() -> MiniTokio {
+        MiniTokio {
+            tasks: VecDeque::new(),
         }
-
-        println!("GOT {:?}", &buf[..n]);
     }
 
-    Ok(())
+    /// Spawn a future onto the mini-tokio instance.
+    fn spawn<F>(&mut self, future: F)
+        where
+            F: Future<Output=()> + Send + 'static,
+    {
+        self.tasks.push_back(Box::pin(future));
+    }
+
+    fn run(&mut self) {
+        let waker = task::noop_waker();
+        let mut cx = Context::from_waker(&waker);
+
+        while let Some(mut task) = self.tasks.pop_front() {
+            if task.as_mut().poll(&mut cx).is_pending() {
+                self.tasks.push_back(task);
+            }
+        }
+    }
 }
